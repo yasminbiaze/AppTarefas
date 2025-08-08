2b6f2174bf49332f258f797ef3709a6ed87d9eff
yasminbiaze <yasmin.b.martins@aluno.senai.br>
2025-08-08 16:33:26 -0300

    Adiciona funcionalidade de gerenciamento de tarefas
    Alterações principais:
    - Atualizado `_Layout.cshtml` para incluir link no menu para "Lista de Tarefas" e ajustado idioma para `pt-br`.
    - Criado `TarefasController` com ação `Index` para gerenciar tarefas.
    - Adicionada classe modelo `Tarefa` com propriedades para representar dados das tarefas.
    - Criada view `Index.cshtml` para exibir a lista de tarefas.

---
+++ AppTarefas/Controllers/TarefasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppTarefas.Controllers
+{
+    public class TarefasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AppTarefas/Models/Tarefa.cs
@@ -0,0 +1,12 @@
+﻿namespace AppTarefas.Models
+{
+    public class Tarefa
+    {
+        //Nome da chave primaria deve ser Nome da Classe + "Id"
+        public int TarefaId { get; set; } //Id é a chave primária
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+        public bool Concluida { get; set; }
+
+    }
+}
